aa337e358ec6f9934cf2b777633fe0e86ac12d0f
sabbirshiam <sabbirshiam@gmail.com>
2020-03-15 18:48:52 +0900

    Behavioral Command Pattern added.

---
+++ src/main/kotlin/patterns/behavior/command/CommandPattern.kt
@@ -0,0 +1,43 @@
+package patterns.behavior.command
+
+/**
+ * The command pattern is used to express a request,
+ * including the call to be made and all of its required parameters, in a command object.
+ * The command may then be executed immediately or held for later use.
+ */
+interface OrderCommand {
+    fun execute()
+}
+
+class OrderAddCommand(val id: Long) : OrderCommand {
+    override fun execute() = println("Adding order with id: $id")
+}
+
+class OrderPayCommand(val id: Long) : OrderCommand {
+    override fun execute() = println("Paying for order with id: $id")
+}
+
+class CommandProcessor {
+
+    private val queue = ArrayList<OrderCommand>()
+
+    fun addToQueue(orderCommand: OrderCommand): CommandProcessor =
+        apply {
+            queue.add(orderCommand)
+        }
+
+    fun processCommands(): CommandProcessor =
+        apply {
+            queue.forEach { it.execute() }
+            queue.clear()
+        }
+}
+
+fun main(args: Array<String>) {
+    CommandProcessor()
+        .addToQueue(OrderAddCommand(1L))
+        .addToQueue(OrderAddCommand(2L))
+        .addToQueue(OrderPayCommand(2L))
+        .addToQueue(OrderPayCommand(1L))
+        .processCommands()
+}
