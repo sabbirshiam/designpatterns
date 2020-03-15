c10a4cf949bbc70eacbf923ccca045d49581503f
sabbirshiam <sabbirshiam@gmail.com>
2020-03-15 18:26:49 +0900

    Builder pattern added.

---
+++ src/main/kotlin/patterns/creational/builder/BuilderPattern.kt
@@ -0,0 +1,81 @@
+package patterns.creational.builder
+
+// Let's assume that Dialog class is provided by external library.
+// We have only access to Dialog public interface which cannot be changed.
+
+class Dialog {
+
+    fun showTitle() = println("showing title")
+
+    fun setTitle(text: String) = println("setting title text $text")
+
+    fun setTitleColor(color: String) = println("setting title color $color")
+
+    fun showMessage() = println("showing message")
+
+    fun setMessage(text: String) = println("setting message $text")
+
+    fun setMessageColor(color: String) = println("setting message color $color")
+
+    fun show() = println("showing dialog $this")
+}
+
+//Builder:
+class DialogBuilder() {
+    constructor(init: DialogBuilder.() -> Unit) : this() {
+        init()
+    }
+
+    private var titleHolder: TextView? = null
+    private var messageHolder: TextView? = null
+
+    fun title(init: TextView.() -> Unit) {
+        titleHolder = TextView().apply { init() }
+    }
+
+    fun message(init: TextView.() -> Unit) {
+        messageHolder = TextView().apply { init() }
+    }
+
+    fun build(): Dialog {
+        val dialog = Dialog()
+
+        titleHolder?.apply {
+            dialog.setTitle(text)
+            dialog.setTitleColor(color)
+            dialog.showTitle()
+        }
+
+        messageHolder?.apply {
+            dialog.setMessage(text)
+            dialog.setMessageColor(color)
+            dialog.showMessage()
+        }
+
+        return dialog
+    }
+
+    class TextView {
+        var text: String = ""
+        var color: String = "#00000"
+    }
+}
+
+fun main(args: Array<String>) {
+    //Function that creates dialog builder and builds Dialog
+    fun dialog(init: DialogBuilder.() -> Unit): Dialog {
+        return DialogBuilder(init).build()
+    }
+
+    val dialog: Dialog = dialog {
+        title {
+            text = "Dialog Title"
+        }
+        message {
+            text = "Dialog Message"
+            color = "#333333"
+        }
+    }
+
+    dialog.show()
+}
